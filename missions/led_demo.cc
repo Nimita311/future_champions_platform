7f6e9e9d8574a434e204a95e9cfa603c25257b30
Haoze Zhang <hzz@hzz.sh>
2024-01-24 17:13:12 +0800

    Add async pulse pulse generator
    Used to support activity indicator LEDs.

--- missions/led_demo.cc
+++ missions/led_demo.cc
@@ -4,6 +4,7 @@
 #include <task.h>
 
 #include "platform/digital_output/digital_output.hh"
+#include "platform/digital_output/pulse_generator.hh"
 
 void halt() {
   while (true) {
@@ -53,5 +54,41 @@ extern "C" __attribute__((weak)) void main_task(const void*) {
     led_green.toggle();
     led_yellow.toggle();
     led_red.toggle();
+
+    vTaskDelay(1000U);
+
+    const auto pg = fcp::platform::PulseGenerator{GPIOB, GPIO_PIN_14};
+    constexpr auto begin_delay_tick = uint32_t{25U};
+    constexpr auto end_delay_tick = uint32_t{500U};
+    for (auto idx = end_delay_tick; idx > begin_delay_tick; idx -= idx / 10U) {
+      pg.pulse();
+      vTaskDelay(idx);
+    }
+    for (auto idx = begin_delay_tick; idx < end_delay_tick; idx += idx / 10U) {
+      pg.pulse();
+      vTaskDelay(idx);
+    }
+
+    vTaskDelay(1000U);
+
+    for (auto idx = 1; idx < 13; idx++) {
+      const auto pulse_width_ms = idx * 100;
+      pg.pulse(pulse_width_ms);
+      vTaskDelay(1000U);
+    }
+
+    vTaskDelay(1000U);
+
+    pg.pulse(portMAX_DELAY);
+    if (!pg.is_active()) {
+      halt();
+    }
+    vTaskDelay(1000U);
+    pg.stop();
+    if (pg.is_active()) {
+      halt();
+    }
+
+    vTaskDelay(1000U);
   }
 }
